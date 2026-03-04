a6c498655cd8dcf4cd1739107c03c70bab97f1be
Josué Cabello <Josuecabellorosa@hotmail.com>
2026-03-04 18:11:59 +0100

    nuevo lote seo

---
+++ seo-system/generador_seo.py
@@ -0,0 +1,21 @@
+import os
+
+with open("plantilla.html","r",encoding="utf8") as f:
+    plantilla = f.read()
+
+with open("mapa_seo.txt","r",encoding="utf8") as f:
+    lineas = f.readlines()
+
+for linea in lineas:
+
+    url,titulo = linea.strip().split("|")
+
+    html = plantilla.replace("{{TITLE}}",titulo)
+    html = html.replace("{{URL}}",url)
+
+    os.makedirs("../"+url,exist_ok=True)
+
+    with open("../"+url+"/index.html","w",encoding="utf8") as f:
+        f.write(html)
+
+print("Páginas generadas correctamente")
